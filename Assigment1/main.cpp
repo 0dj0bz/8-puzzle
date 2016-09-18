86ab18a3e4c3c7b56dcb6a6f8604f667e9c02925
Rob Abbott <rob@perceival.com>
2016-09-18 12:32:31 -0400

    Initial Commit

---
+++ Assigment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assigment1
+//
+//  Created by Rob Abbott on 9/18/16.
+//  Copyright © 2016 Rob Abbott. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
