34ad0b2d9b4a08b9943f9af3d8bfd8f004ba8a88
Rob Abbott <rob@perceival.com>
2016-09-21 03:32:56 -0400

    Working version of 8-puzzle solver; finds optimal path for first sample.

--- Assigment1/main.cpp
+++ Assigment1/main.cpp
@@ -1,21 +1,24 @@
 //
 //  main.cpp
-//  Assigment1
+//  ITCS 8150 Programming Assigment1
 //
-//  Created by Rob Abbott on 9/18/16.
-//  Copyright © 2016 Rob Abbott. All rights reserved.
+//  Created by Rob Abbott - 800835197 on 9/18/16.
+//  University of North Carolina at Charlotte
 //
 
+
+// includes
 #include <iostream>
 #include <vector>
+#include <queue>
 
+// namespaces
 using namespace std;
 
 
-
-
-
-class Node {
+// State class
+// This is the class that we use to represent a state in the 8-puzzle problem
+class State {
 public:
     int values[3][3] = { {0,0,0}, {0,0,0}, {0,0,0} };
     int cols[3][3] = { {0,0,0}, {0,0,0}, {0,0,0} };
@@ -25,49 +28,57 @@ public:
     int g_n = {0};
     int h_n = {0};
     int zero[2] = {0,0};  // 0 - row, 1 - col
-    Node *parent = NULL;
+    State *parent = NULL;
 
-    
-    Node ();
-    Node (int state[3][3]);
-    Node (Node const &);
-    string print();
+    State () {};
+    State (int state[3][3]);
+    State (State const &);
+    void print();
     void calculateRelativeRowColPositions (int targetState[3][3]);
     void calculateManhattanDist ();
     void changeState (int op);
     
-    vector<Node> createChildren();
+    vector<State> createChildStates();
 
 };
 
+
 // create a new Node with a provided state
-Node::Node(int state[3][3]) {
+State::State(int state[3][3]) {
     for (int i=0;i<3;i++)
-        for (int j=0;j<3;j++)
+        for (int j=0;j<3;j++) {
             values[i][j] = state[i][j];
+            
+            // while we're initializing the state, if we find the zero, save that info
+            if (values[i][j] == 0) {
+                zero[0] = i;
+                zero[1] = j;
+            }
+        }
+    calculateManhattanDist();
 }
 
-// create a new node as a replica of an existing node
+// create a new state as a replica of an existing state
 
-Node::Node(Node const &pnode) {
+State::State(State const &pstate) {
     
     for (int i=0;i<3;i++)
         for (int j=0;j<3;j++) {
-            values[i][j] = pnode.values[i][j];
-            cols[i][j] = pnode.cols[i][j];
-            rows[i][j] = pnode.rows[i][j];
-            op = pnode.op;
-            f_n = pnode.f_n;
-            g_n = pnode.g_n;
-            h_n = pnode.h_n;
-            zero[0] = pnode.zero[0];
-            zero[1] = pnode.zero[1];
-            parent = pnode.parent;
+            values[i][j] = pstate.values[i][j];
+            cols[i][j] = pstate.cols[i][j];
+            rows[i][j] = pstate.rows[i][j];
+            op = pstate.op;
+            f_n = pstate.f_n;
+            g_n = pstate.g_n;
+            h_n = pstate.h_n;
+            zero[0] = pstate.zero[0];
+            zero[1] = pstate.zero[1];
+            parent = pstate.parent;
             
         }
 }
 
-void Node::changeState (int op) {
+void State::changeState (int op) {
     
     switch (op) {
         case (1): { //right
@@ -155,7 +166,7 @@ void Node::changeState (int op) {
 }
 
 // returns a vector<> of child nodes derived from the parent
-vector<Node> Node::createChildren() {
+vector<State> State::createChildStates() {
     
     vector<int> ops;
 
@@ -177,27 +188,30 @@ vector<Node> Node::createChildren() {
         ops.push_back(-1);
     
     // now generate the children
-    vector<Node> childNodes;
+    vector<State> childStates;
     
     for (int i=0;i<ops.size();i++) {
 
         if (ops[i] == -(this->op) ) // if this op undoes the parent of the new node, skip it
             continue;
         
-        Node childNode (*this);  // initialize the new child's values with the values in this node
-        childNode.parent = this; // set the new child's parent to the current node
+        State childState (*this);  // initialize the new child's values with the values in this node
+        childState.parent = this; // set the new child's parent to the current node
         
-        childNode.op = ops[i];
-        childNode.changeState(ops[i]);
+        childState.op = ops[i];
+        childState.changeState(ops[i]);
+        childState.calculateManhattanDist();
+        childState.g_n++; // increase the depth by one
+        childState.f_n = childState.g_n + childState.h_n;
         
-        childNodes.push_back(childNode);
+        childStates.push_back(childState);
     }
     
-    return childNodes;
+    return childStates;
     
 }
 
-void Node::calculateRelativeRowColPositions(int targetState[3][3]) {
+void State::calculateRelativeRowColPositions(int targetState[3][3]) {
     for (int i=0;i<3;i++)
         for (int j=0;j<3;j++)
             for (int k=0;k<3;k++)
@@ -211,7 +225,7 @@ void Node::calculateRelativeRowColPositions(int targetState[3][3]) {
                 }
 }
 
-void Node::calculateManhattanDist() {
+void State::calculateManhattanDist() {
     
     h_n = 0;
     
@@ -227,7 +241,7 @@ void Node::calculateManhattanDist() {
 
 
 
-string Node::print() {
+void State::print() {
     
     string outBuf = "{";
     for (int i=0;i<3;i++) {
@@ -242,27 +256,92 @@ string Node::print() {
     }
     outBuf += "}";
     
-    return outBuf;
+    cout << "state: " << outBuf << " : f(n): " << f_n << " g(n): " << g_n << " h(n): " << h_n << endl;
+    
+    return;
 }
 
 
 int main(int argc, const char * argv[]) {
 
+    // declarations
+    
+    // define our initial and final states
+    // these should be dynamic
+    
+    int _initialState [3][3] = {{2,8,3},{1,6,4},{7,0,5}};
+    int _finalState [3][3] =   {{1,2,3},{8,6,4},{7,5,0}};
 
+    // this is the lambda function used by our priority queue to order the elements
+    auto compare = [](State state1, State state2) -> bool { return state1.h_n > state2.h_n; };
     
-    int initialState [3][3] = {{2,8,3},{1,6,4},{7,0,5}};
-    int finalState [3][3] =   {{1,2,3},{8,6,4},{7,5,0}};
+    // the priority queue that contains the states of the 8-puzzle
+    priority_queue<State, vector<State>, decltype(compare)> workingPath {compare};
+
+    // indicator that we've reached the goal state
+    bool goalAchieved = false;
     
-    Node myNode( initialState);
-    myNode.zero[0] = 2; myNode.zero[1] = 1;
+    // save the goalState so that we can print the shortest path
+    State *goalState;
     
-    myNode.calculateRelativeRowColPositions(finalState);
-    myNode.calculateManhattanDist();
+    // generate the initialState for the 8-puzzle
+    State initialState(_initialState);
     
-    vector<Node> childNodes = myNode.createChildren();
+    // calculate the positions of each of the tiles relative to the target state
+    initialState.calculateRelativeRowColPositions(_finalState);
     
-    cout << myNode.h_n << endl;
-    cout << myNode.print() << endl;
+    // calculate the L1 distance for the initial state
+    initialState.calculateManhattanDist();
+    initialState.f_n = initialState.g_n + initialState.h_n;
+    
+    // push this onto the priority queue as the root
+    workingPath.push(initialState);
+    
+    // now process the state space until we're out of states or we reach the goal state
+    
+    cout << "Search space to find goal:" << endl;
+    while(!workingPath.empty()) {
+
+        // retrieve the lowest-cost state from the priority queue
+        State *currentState = new State(workingPath.top());
+        workingPath.pop();
+        
+        currentState->print();
+        
+        // check to see if we've reached the goal state
+        // if so, break out
+        if (currentState->h_n == 0) {
+            goalState = currentState;
+            goalAchieved = true;
+            break;
+        }
+        
+        // generate the child states for the current state
+        vector<State> childStates = currentState->createChildStates();
+    
+        // push the child states into the priority queue
+        for (int i=0;i<childStates.size();i++)
+            workingPath.push(childStates[i]);
+          
+    }
+    
+    if (goalAchieved)
+        cout << "goal reached" << endl;
+    else
+        cout << "goal not reached" << endl;
+    
+    // print out shortest path
+    
+    State * aState;
+    
+    aState = goalState;
+    
+    // walk the path from the goal state back to the initial state
+    cout << "Optimal path from goal state to initial state:" << endl;
+    while (aState != NULL) {
+        aState->print();
+        aState = aState->parent;
+    }
     
     return 0;
 }
